e1641ffcf260b11586b6cb5fbb92f7cf9664cab2
shisui <gwk24yt@gmail.com>
2021-09-02 20:49:16 +0530

    help

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ const cron = require('node-cron');
 module.exports = (client, token, disableConsole = false) => {
     return new Promise((resolve, reject) => {
         // Invalid Parameter handling
-        if (!client || !"isReady" in client || typeof (client) !== "object") return reject("Invalid Client was provided");
+        if (!client || typeof (client) !== "object") return reject("Invalid Client was provided");
         if (!token || typeof (token) !== "string" || token.length !== 36) return reject("Invalid Token was provided");
-        if ((typeof (timeout) !== "number" && typeof (timeout) !== "bigint") || timeout < 300000) return reject("Invalid Timeout, Timeout should be in milli seconds and at least greater than 300000");
         if (typeof (disableConsole) !== "boolean") return reject("Invalid disableConsole type, it should be either false or true");
 
         // If client is ready i.e. it have its data
@@ -21,7 +20,7 @@ module.exports = (client, token, disableConsole = false) => {
         }
         else {
             client.once('ready', () => cron.schedule('1h', sendCount(client, token, disableConsole)));
-        }   
+        }
 
         resolve("Auto Server Count Sender is activated");
     })
